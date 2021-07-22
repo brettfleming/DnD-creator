1bc51ee903843ce8d765b1aaf761820a520bea3f
JacobBeckfeld <jacbeck456@gmail.com>
2021-07-22 14:24:06 -0500

    added models and their relationships

---
+++ models/user.js
@@ -0,0 +1,54 @@
+const sequelize = require("../config/connection");
+const { Model, DataTypes} = require(sequelize);
+const bycrypt = require("bcrypt");
+
+class User extends Model {}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTERGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8],
+            },
+        },
+    },
+    {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bycrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            beforeUpdate: async (updatedUserData) => {
+                updatedUserData.password = await bycrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "user"
+    }
+);
+
+module.exports = User;
