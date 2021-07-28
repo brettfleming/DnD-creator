10eebc485645b37443f7571883e6734c5f25c7b5
Yueshia Vue <yueshiavue@Yueshias-MacBook-Pro.local>
2021-07-28 17:19:45 -0500

    testing

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
+const PORT = process.env.PORT || 3001;
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-const seed = require('./seeds/index');
+
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -33,7 +33,7 @@ app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
-seed();
+
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
   });
