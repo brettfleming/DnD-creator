e618c3a4bd39c14b5daa3d16f244cbd6272279e0
Joshua Chosay <jjchosay@gmail.com>
2021-07-28 13:27:08 -0500

    Dice funtionality working!

--- public/js/create.js
+++ public/js/create.js
@@ -1,3 +1,6 @@
+const throwdice = document.getElementById('throwdice');
+    throwdice.addEventListener('click', throwdembonez);
+
 const createCharacter = async (event) => {
     event.preventDefault();
     const character_name = document.getElementById('character_name').value;
@@ -14,7 +17,6 @@ const createCharacter = async (event) => {
     console.log(character_race);
 
 
-
     if (character_name && character_background && character_class && character_race && character_alignment && str && dex && chr && cons && int && wis) {
         const response = await fetch('/api/character/create', {
             method: 'POST',
@@ -29,6 +31,31 @@ const createCharacter = async (event) => {
         }
     }
 }
+
+function throwdembonez () {
+    console.log("Entering throw function");
+    const strthrow = document.getElementById('str');
+    const dexthrow = document.getElementById('dex');
+    const chrthrow = document.getElementById('chr');
+    const conthrow = document.getElementById('const');
+    const intthrow = document.getElementById('int');
+    const wisthrow = document.getElementById('wis');
+
+    let strRand = Math.floor(Math.random() * 20);
+    let dexRand = Math.floor(Math.random() * 20);
+    let chrRand = Math.floor(Math.random() * 20);
+    let conRand = Math.floor(Math.random() * 20);
+    let intRand = Math.floor(Math.random() * 20);
+    let wisRand = Math.floor(Math.random() * 20);
+
+    strthrow.setAttribute('value',strRand);
+    dexthrow.setAttribute('value',dexRand);
+    chrthrow.setAttribute('value',chrRand);
+    conthrow.setAttribute('value',conRand);
+    intthrow.setAttribute('value',intRand);
+    wisthrow.setAttribute('value',wisRand);
+}
+
 document
   .querySelector('.create-character')
   .addEventListener('submit', createCharacter);
